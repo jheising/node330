1605dac0c3efcdc5a4e81a3ec5b91fffadcbe5c4
Jim Heising <jheising@redfoundry.com>
2013-06-23 22:53:44 -0700

    Fixed some path issues

--- node330.js
+++ node330.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 var argv = require('optimist').argv;
-var forever = require('forever');
 var path = require("path");
 var _ = require("underscore");
 var node330 = require("./lib/node330");
@@ -13,30 +12,6 @@ if(_.isUndefined(argv.program))
 }
 else
 {
-    if(_.isUndefined(argv.daemon))
-    {
-        var programPath = path.normalize("./" + argv.program);
-        var node330Program = node330.run(programPath);
-    }
-    else
-    {
-        var daemon = new (forever.Monitor)(__filename, {
-            max: 1,
-            silent: false,
-            options: ["--program", argv.program],
-            /*watch: true,
-            watchDirectory: __dirname,
-            watchIgnoreDotFiles: true,
-            logFile: "./logs/daemon.log"*/
-        });
-
-        daemon.on('restart', function () {
-            console.log('node330 daemon has restarted.');
-        });
-        daemon.on('exit', function () {
-            console.log('node330 daemon has exited.');
-        });
-
-        daemon.start();
-    }
+    var programPath = path.normalize("./" + argv.program);
+    node330.run(programPath);
 }
