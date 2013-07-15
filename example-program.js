5038a8a1918ab768c704cfe285a1b5fa2d162cce
Jim Heising <jheising@redfoundry.com>
2013-07-15 15:54:33 -0700

    Changed way components get loaded

--- example-program.js
+++ example-program.js
@@ -1,12 +1,25 @@
+// Our setup function. This will be run once at the beginning of the program.
 module.exports.setup = function(
 	node330,            // The main node330 object
 	mainPID,            // A PID loop. Anything that starts or ends with PID will be injected as a PID
-	tempProbe)          // A Temp Probe. Anything that begins or ends with temp will be
+	tempProbe,          // A Temp Probe. Anything that begins or ends with temp will be injected as a Temperature probe
+	heaterPower)        // Any other type of parameter will be defined as a standard numeric value
 {
 
 }
 
+// Our loop function. This will be run at an interval of once per second.
 module.exports.loop = function(node330)
 {
 
+}
+
+function simulateTempChange(tempProbe)
+{
+
+}
+
+function setHeaterPower()
+{
+	// Send a command to a sensor or component that will update my heater value.
 }
--- node330.js
+++ node330.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var agent = require('webkit-devtools-agent');
+
 var argv = require('optimist').argv;
 var path = require("path");
 var _ = require("underscore");
@@ -7,11 +9,10 @@ var node330 = require("./lib/node330");
 
 if(_.isUndefined(argv.program))
 {
-    console.log("Must specify a program to run with --program <program_filename>");
+    console.log("Must specify a program to run with --program <program_filename or npm_name>");
     return;
 }
 else
 {
-    var programPath = path.normalize("./" + argv.program);
-    node330.run(programPath);
+    node330.run(argv.program);
 }
