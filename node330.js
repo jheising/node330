614bed0e4f03bc8b8f23ef748cd7d680a07f972f
Jim Heising <jheising@redfoundry.com>
2013-07-17 23:33:19 -0700

    Removed mem leak detection code

--- node330.js
+++ node330.js
@@ -1,15 +1,8 @@
 #!/usr/bin/env node
-
-var memLeakAgent;
 var argv = require('optimist').argv;
 var path = require("path");
 var _ = require("underscore");
 var node330 = require("./lib/node330");
-
-if(_.isUndefined(argv.debug))
-{
-    memLeakAgent = require('webkit-devtools-agent');
-}
 
 if(_.isUndefined(argv.program))
 {
