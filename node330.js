c4f07623ab54878ea8caf3015d3aa9b4583b7912
Jim Heising <jheising@redfoundry.com>
2013-07-26 11:07:10 -0700

    Added remoteServerViewer viewer

--- node330.js
+++ node330.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
 var argv = require('optimist').argv;
 var path = require("path");
+var fs = require("fs");
 var _ = require("underscore");
 var node330 = require("./lib/node330");
 
-if(_.isUndefined(argv.program))
+var configFilename = process.cwd() + "/node330-config.json";
+var config = {};
+
+// Is there a config file?
+if(fs.existsSync(configFilename))
+{
+	config = require(configFilename);
+}
+
+config = _.defaults(config, {
+	program: argv.program
+});
+
+// Save our config, in case any haven't been initialized yet
+var configString = JSON.stringify(config, null, 4);
+fs.writeFileSync(configFilename, configString);
+
+if(_.isUndefined(config.program))
 {
     console.log("Must specify a program to run with --program <program_filename or npm_name>");
     return;
 }
 else
 {
-    node330.run(argv.program);
+    node330.run(config);
 }
