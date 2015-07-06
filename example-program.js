1ba63d326dca1e3f3286c18f7753fd95520c5f90
Jim Heising <jheising@gmail.com>
2015-07-06 16:00:01 -0700

    Updated example program

--- example-program.js
+++ example-program.js
@@ -1,25 +1,35 @@
 // Our setup function. This will be run once at the beginning of the program.
-module.exports.setup = function(
-	node330,            // The main node330 object
-	mainPID,            // A PID loop. Anything that starts or ends with PID will be injected as a PID
-	tempProbe,          // A Temp Probe. Anything that begins or ends with temp will be injected as a Temperature probe
-	heaterPower)        // Any other type of parameter will be defined as a standard numeric value
-{
+module.exports.setup = function (node330,
+                                 runSwitch,
+                                 mainPID,
+                                 tempSensor,
+                                 tempSetPoint
+) {
+    node330.addViewer(node330.consoleViewer());
+    node330.addViewer(node330.restViewer());
+    node330.addViewer(node330.webViewer());
 
+    node330.exposeVirtualComponentToViewers(runSwitch, false);
+    node330.exposeVirtualComponentToViewers(tempSensor, true);
+    node330.exposeVirtualComponentToViewers(tempSetPoint, false);
+
+    tempSetPoint.setValueType(node330.valueTypes.TEMP_IN_C);
+
+    mainPID.setProportionalGain(1);
+    mainPID.setIntegralGain(1);
 }
 
 // Our loop function. This will be run at an interval of once per second.
-module.exports.loop = function(node330)
-{
+module.exports.loop = function (node330, runSwitch, tempSensor, tempSetPoint, mainPID) {
+    if (runSwitch.isOn()) {
+        // Tell our PID the value we want to reach
+        mainPID.setDesiredValue(tempSetPoint.getValue());
 
-}
+        // Calculate our control value
+        var controlValue = Math.round(mainPID.update(tempSensor.getValue()));
 
-function simulateTempChange(tempProbe)
-{
-
-}
-
-function setHeaterPower()
-{
-	// Send a command to a sensor or component that will update my heater value.
+        // Simulate a temperature change based on our control value
+        var simulatedTemperature = Number((tempSensor.getValue() + (controlValue * 0.20) - (tempSensor.getValue() * 0.10)).toFixed(3));
+        tempSensor.setValue(simulatedTemperature);
+    }
 }
